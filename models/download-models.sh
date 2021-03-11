a222653065fc6ba78b8991de7a612c79ed126a4c
Jerin Philip <jphilip@ed.ac.uk>
2021-03-11 11:44:53 +0000

    Moving download and extraction attempts to shell script with guarded checks

--- models/download-models.sh
+++ models/download-models.sh
@@ -7,8 +7,10 @@ OUTPUT_DIR="deen"
 mkdir -p ${OUTPUT_DIR}
 
 if [ -f "$FILE" ]; then
-    echo "File already downloaded."
+    echo "File ${FILE} already downloaded."
 else
     wget --quiet --continue $URL/${FILE}
     tar xf $FILE -C $OUTPUT_DIR/
+    # wasm build doesnt support zipped input 
+    ( cd models/deen/ende.student.tiny11 && gunzip lex.s2t.gz )
 fi
