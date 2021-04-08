fd0f3b2263170300556027a5b2d0f5f7e19e1b9d
Nikolay Bogoychev <nheart@gmail.com>
2021-04-08 16:40:32 +0100

    Fix test arguments to be consistent across binary and non-binary versions

--- tests/basic/test_service-cli_intgemm_8bit.bytearrayload.cpu-threads.4.sh
+++ tests/basic/test_service-cli_intgemm_8bit.bytearrayload.cpu-threads.4.sh
@@ -30,6 +30,7 @@ ARGS=(
         $BRT_MODELS/deen/ende.student.tiny11/vocab.deen.spm 
         $BRT_MODELS/deen/ende.student.tiny11/vocab.deen.spm
     --ssplit-mode paragraph
+    --alignment soft
     --beam-size 1
     --skip-cost
     --shortlist $BRT_MODELS/deen/ende.student.tiny11/lex.s2t.bin 50 50
