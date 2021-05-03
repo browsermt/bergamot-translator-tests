dfb5016c214778742d58c8978b8f2358bc6cb133
Jerin Philip <jphilip@ed.ac.uk>
2021-05-03 17:17:00 +0000

    USE_MKL checks MKL; setup fails and tests skip if no MKL

--- run_brt.sh
+++ run_brt.sh
@@ -61,7 +61,7 @@ $BRT_MARIAN/app/bergamot-translator-app --build-info all 2> $BRT_ROOT/cmake.log
 # Check Marian compilation settings
 export BRT_MARIAN_BUILD_TYPE=$(cat $BRT_ROOT/cmake.log        | grep "CMAKE_BUILD_TYPE=" | cut -f2 -d=)
 export BRT_MARIAN_COMPILER=$(cat $BRT_ROOT/cmake.log          | grep "CMAKE_CXX_COMPILER=" | cut -f2 -d=)
-export BRT_MARIAN_USE_MKL=$(cat $BRT_ROOT/cmake.log           | egrep "COMPILE_CPU=(ON|on|1)")
+export BRT_MARIAN_USE_MKL=$(cat $BRT_ROOT/cmake.log           | egrep "USE_MKL=(ON|on|1)")
 export BRT_MARIAN_USE_CUDA=$(cat $BRT_ROOT/cmake.log          | egrep "COMPILE_CUDA=(ON|on|1)")
 export BRT_MARIAN_USE_CUDNN=$(cat $BRT_ROOT/cmake.log         | egrep "USE_CUDNN=(ON|on|1)")
 export BRT_MARIAN_USE_SENTENCEPIECE=$(cat $BRT_ROOT/cmake.log | egrep "USE_SENTENCEPIECE=(ON|on|1)")
---
+++ tests/basic/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Skip if requirements are not met
+if [ ! $BRT_MARIAN_USE_MKL ]; then
+    echo "Bergamot translator is not compiled with CPU" 1>&2
+    exit 100
+fi
+
+exit 1
+
