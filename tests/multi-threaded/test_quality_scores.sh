16cfb59213ec913b53543ec329a3c265f11ea557
Jerin Philip <jphilip@ed.ac.uk>
2021-05-31 21:39:41 +0000

    Adding annotation sentences tests

--- tests/multi-threaded/test_quality_scores.sh
+++ tests/multi-threaded/test_quality_scores.sh
@@ -10,22 +10,6 @@ set -eo pipefail;
 
 source "${BRT_TOOLS}/functions.sh"
 
-ARGS=(
-    -m $BRT_TEST_PACKAGE_EN_DE/model.intgemm.alphas.bin
-    --vocabs 
-        $BRT_TEST_PACKAGE_EN_DE/vocab.deen.spm 
-        $BRT_TEST_PACKAGE_EN_DE/vocab.deen.spm
-    --shortlist $BRT_TEST_PACKAGE_EN_DE/lex.s2t 50 50
-    --alignment soft
-    --beam-size 1
-    --skip-cost
-    --gemm-precision ${GEMM_PRECISION}
-    --cpu-threads 4
-    --max-length-break 1024
-    --mini-batch-words 1024
-    -w 128
-)
-
 # Generate output specific to hardware.
 OUTFILE=$BRT_DATA/simple/bergamot/$(brt_outfile "quality-scores")
 EXPECTED=$BRT_DATA/simple/bergamot/$(brt_expected "quality-scores")
---
+++ tests/multi-threaded/test_response_target_sentences.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#####################################################################
+# SUMMARY: Run tests for service-cli
+# AUTHOR: jerinphilip 
+# TAGS: full
+#####################################################################
+
+set -eo pipefail;
+
+source "${BRT_TOOLS}/functions.sh"
+
+# Generate output specific to hardware.
+OUTFILE=$BRT_DATA/simple/bergamot/$(brt_outfile "response-target-sentences")
+EXPECTED=$BRT_DATA/simple/bergamot/$(brt_expected "response-target-sentences")
+${BRT_MARIAN}/app/bergamot --bergamot-mode test-response-target-sentences "${BRT_FILE_ARGS[@]}" < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE 
+
+$BRT_TOOLS/diff.sh $OUTFILE $EXPECTED 
+exit 0
