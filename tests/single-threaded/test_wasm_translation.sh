09952089bdd08a80bf02b6b4a5547cb366f44733
Jerin Philip <jphilip@ed.ac.uk>
2021-06-02 16:42:22 +0000

    Updating WASM output to use wasm data folder

--- tests/single-threaded/test_wasm_translation.sh
+++ tests/single-threaded/test_wasm_translation.sh
@@ -9,31 +9,12 @@
 
 set -eo pipefail;
 
-prefix=intgemm_8bit
-
-ARGS=(
-    -m $BRT_TEST_PACKAGE_EN_DE/model.intgemm.alphas.bin
-    --vocabs 
-        $BRT_TEST_PACKAGE_EN_DE/vocab.deen.spm 
-        $BRT_TEST_PACKAGE_EN_DE/vocab.deen.spm
-    --shortlist $BRT_TEST_PACKAGE_EN_DE/lex.s2t 50 50
-    --beam-size 1
-    --skip-cost
-    --int8shiftAlphaAll
-    --max-length-break 1024
-    --mini-batch-words 1024
-    -w 128
-
-    --ssplit-mode paragraph
-    --check-bytearray false
-    --cpu-threads 0
-)
-
 # Generate output specific to hardware.
-OUTFILE="bergamot.$prefix.$BRT_INSTRUCTION.out"
-${BRT_MARIAN}/app/bergamot --bergamot-mode wasm "${ARGS[@]}" < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
+OUTFILE=${BRT_DATA}/simple/bergamot/$(brt_outfile "wasm")
+EXPECTED=${BRT_DATA}/simple/bergamot/$(brt_expected "wasm")
+${BRT_MARIAN}/app/bergamot --bergamot-mode wasm ${BRT_BYTEARRAY_ARGS} < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
 
 # This used to be provided via stdin: < ${BRT_DATA}/simple/bergamot.in  but the bergamot-translator-app doesn't accept stdin text
 # Compare with output specific to hardware.
-$BRT_TOOLS/diff.sh $OUTFILE bergamot.$prefix.$BRT_INSTRUCTION.expected > $prefix.$BRT_INSTRUCTION.diff
+$BRT_TOOLS/diff.sh $OUTFILE $EXPECTED 
 exit 0
