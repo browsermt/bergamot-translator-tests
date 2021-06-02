f7bfcdee29b0dc71cf67dbb60ab8fff56625743a
Jerin Philip <jphilip@ed.ac.uk>
2021-06-02 18:34:15 +0100

    Bulk update script for ssse3 as well, adding ssse3 outputs on lofn

--- tools/bulk-update.sh
+++ tools/bulk-update.sh
@@ -3,7 +3,7 @@
 data=$1
 precision="int8shiftAlphaAll"
 
-for arch in avx2 avx512bw avx512vnni; do
+for arch in ssse3 avx2 avx512bw avx512vnni; do
     dir="$data/$precision/$arch"
     find "$dir" -iname "*.out" | sed 's/\(.*\)\.out$/cp \1.out \1.expected/' | xargs -I% echo %
 done
