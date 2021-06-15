7bcbdd15fbf12fd20b2daa024c3635c293cb8a7c
Jerin Philip <jphilip@ed.ac.uk>
2021-06-15 15:41:39 +0100

    ssplit: prefix-files download, test-cases
    Adds ssplit non-breaking-prefix file among "model" downloads. Uses the
    said file in the common args for both File based and ByteArray based
    loads for bergamot translator.
    
    To test this ssplit prefix file, we require a file that is different
    from bergamot, where non-breaking prefixes appear, and therefore a test
    case using ssplit's English tests are translated through
    bergamot-translator and the source-sentences in the Response information
    is asserted to be consistent with expected output.

---
+++ tests/multi-threaded/test_ssplit_with_file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################################################
+# SUMMARY: Run tests for service-cli
+# AUTHOR: jerinphilip 
+# TAGS: full
+#####################################################################
+
+set -eo pipefail;
+
+# Generate output specific to hardware.
+OUTFILE=$BRT_DATA/ssplit/sample.en.out
+EXPECTED=$BRT_DATA/ssplit/sample.en.expected
+${BRT_MARIAN}/bergamot-test --bergamot-mode test-response-source-sentences ${BRT_FILE_ARGS} < ${BRT_DATA}/ssplit/sample.en > $OUTFILE 
+
+# Source sentences are deterministic.
+$BRT_TOOLS/diff.sh $OUTFILE $EXPECTED 
+
+OUTFILE=$BRT_DATA/ssplit/sample.en.bytearray.out
+EXPECTED=$BRT_DATA/ssplit/sample.en.expected
+${BRT_MARIAN}/bergamot-test --bergamot-mode test-response-source-sentences ${BRT_BYTEARRAY_ARGS} < ${BRT_DATA}/ssplit/sample.en > $OUTFILE 
+
+# Source sentences are deterministic.
+$BRT_TOOLS/diff.sh $OUTFILE $EXPECTED 
+exit 0
