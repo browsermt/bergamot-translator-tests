528e6b34e1f9f2ce2081e6e4ab235826a55824d2
Jerin Philip <jphilip@ed.ac.uk>
2021-08-02 10:15:18 +0000

    Adding equivalent test for WASM cache

---
+++ tests/single-threaded/test_translation_cache_wasm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#####################################################################
+# SUMMARY: Run tests for translation-cache
+# AUTHOR: jerinphilip 
+# TAGS: wasm
+#####################################################################
+
+set -eo pipefail;
+
+# There are no output / expected files here. Just a check if the cache-stats reflect that the cache works correctly, ABORT contained within the test-app if not.
+
+${BRT_MARIAN}/bergamot-test --bergamot-mode test-translation-cache-wasm ${BRT_FILE_ARGS} < ${BRT_DATA}/simple/bergamot/input.txt
+
+exit 0
