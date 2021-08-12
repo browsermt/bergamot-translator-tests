55afad1d0b6bf7c9d0c6e576b3b472409697b5b1
Jerin Philip <jphilip@ed.ac.uk>
2021-08-12 12:17:15 +0000

    Downloading en-es models

--- models/download-models.sh
+++ models/download-models.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
+# Model download
+
+function download-archive {
+    URL=$1
+    MODEL=$2
+    FILE="${MODEL}.tar.gz"
+    OUTPUT_DIR=$3
+    mkdir -p ${OUTPUT_DIR}
+    if [ -f "$FILE" ]; then
+        echo "File ${FILE} already downloaded."
+    else
+        echo "Downloading ${FILE}"
+        wget --quiet --continue $URL/${FILE}
+        tar xf $FILE -C $OUTPUT_DIR/
+        # wasm build doesnt support zipped input 
+        ( cd ${OUTPUT_DIR}/${MODEL} && gunzip -f lex.s2t.gz )
+    fi
+}
+
+function download-ssplit-prefix-file {
+    SLANG=$1
+    OUTPUT_DIR=$2
+    MODEL=$3
+    wget "https://raw.githubusercontent.com/ugermann/ssplit-cpp/master/nonbreaking_prefixes/nonbreaking_prefix.${SLANG}" \
+        -O "${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.${SLANG}" || exit 1
+    test -f "${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.${SLANG}" || exit 1
+}
+
+# de-en
 URL="http://data.statmt.org/bergamot/models/deen"
 MODEL="ende.student.tiny.for.regression.tests"
-FILE="${MODEL}.tar.gz"
 OUTPUT_DIR="deen"
 
-mkdir -p ${OUTPUT_DIR}
-
-if [ -f "$FILE" ]; then
-    echo "File ${FILE} already downloaded."
-else
-    echo "Downloading ${FILE}"
-    wget --quiet --continue $URL/${FILE}
-    tar xf $FILE -C $OUTPUT_DIR/
-    # wasm build doesnt support zipped input 
-    ( cd ${OUTPUT_DIR}/${MODEL} && gunzip -f lex.s2t.gz )
-fi
+download-archive $URL $MODEL $OUTPUT_DIR
 
 test -f ${OUTPUT_DIR}/${MODEL}/vocab.deen.spm || exit 1
 test -f ${OUTPUT_DIR}/${MODEL}/model.intgemm.alphas.bin || exit 1
 test -f ${OUTPUT_DIR}/${MODEL}/lex.s2t || exit 1
 
-# Get ssplit non-breaking prefix file. 
-# TODO: Bundle this in the archive.
-
-wget https://raw.githubusercontent.com/ugermann/ssplit-cpp/master/nonbreaking_prefixes/nonbreaking_prefix.en -O ${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.en || exit 1
-test -f ${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.en || exit 1
-
+download-ssplit-prefix-file en $OUTPUT_DIR $MODEL
 python3 ../tools/patch-marian-for-bergamot.py --config-path ${OUTPUT_DIR}/$MODEL/config.intgemm8bitalpha.yml --ssplit-prefix-file nonbreaking_prefix.en
 
+# en->es
+
+URL="http://data.statmt.org/bergamot/models/esen/"
+MODEL="enes.student.tiny11"
+OUTPUT_DIR="enes"
+
+download-archive $URL $MODEL $OUTPUT_DIR
+download-ssplit-prefix-file en $OUTPUT_DIR $MODEL
+python3 ../tools/patch-marian-for-bergamot.py --config-path ${OUTPUT_DIR}/$MODEL/config.intgemm8bitalpha.yml --ssplit-prefix-file nonbreaking_prefix.en
+
+# en->es
+
+URL="http://data.statmt.org/bergamot/models/esen/"
+MODEL="esen.student.tiny11"
+OUTPUT_DIR="esen"
+
+download-archive $URL $MODEL $OUTPUT_DIR
+download-ssplit-prefix-file es $OUTPUT_DIR $MODEL
+python3 ../tools/patch-marian-for-bergamot.py --config-path ${OUTPUT_DIR}/$MODEL/config.intgemm8bitalpha.yml --ssplit-prefix-file nonbreaking_prefix.es
