8c8d4136b3c5589817dcbc94928753f36b1ca262
Felipe C. Dos Santos <felipe.santos.k@gmail.com>
2021-08-23 00:13:21 -0300

    Add enet model for qualtiy estimator test

--- models/download-models.sh
+++ models/download-models.sh
@@ -13,7 +13,7 @@ else
     echo "Downloading ${FILE}"
     wget --quiet --continue $URL/${FILE}
     tar xf $FILE -C $OUTPUT_DIR/
-    # wasm build doesnt support zipped input 
+    # wasm build doesnt support zipped input
     ( cd ${OUTPUT_DIR}/${MODEL} && gunzip -f lex.s2t.gz )
 fi
 
@@ -21,11 +21,25 @@ test -f ${OUTPUT_DIR}/${MODEL}/vocab.deen.spm || exit 1
 test -f ${OUTPUT_DIR}/${MODEL}/model.intgemm.alphas.bin || exit 1
 test -f ${OUTPUT_DIR}/${MODEL}/lex.s2t || exit 1
 
-# Get ssplit non-breaking prefix file. 
+# Get ssplit non-breaking prefix file.
 # TODO: Bundle this in the archive.
 
 wget https://raw.githubusercontent.com/ugermann/ssplit-cpp/master/nonbreaking_prefixes/nonbreaking_prefix.en -O ${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.en || exit 1
 test -f ${OUTPUT_DIR}/${MODEL}/nonbreaking_prefix.en || exit 1
 
-wget https://raw.githubusercontent.com/felipesantosk/students/quality_model/eten/enet.quality.lr/quality_model.bin -O ${OUTPUT_DIR}/${MODEL}/quality_model.bin || exit 1
+URL="http://data.statmt.org/bergamot/models/eten"
+MODEL="enet.student.tiny11"
+FILE="${MODEL}.tar.gz"
+OUTPUT_DIR="enet"
 
+mkdir -p ${OUTPUT_DIR}
+
+if [ -f "${FILE}" ]; then
+    echo "File ${FILE} already downloaded."
+else
+    echo "Downloading ${FILE}"
+    wget --quiet --continue ${URL}/${FILE}
+    tar xf ${FILE} -C ${OUTPUT_DIR}/
+fi
+
+wget https://raw.githubusercontent.com/felipesantosk/students/quality_model/eten/enet.quality.lr/quality_model.bin -O ${OUTPUT_DIR}/${MODEL}/quality_model.bin || exit 1
