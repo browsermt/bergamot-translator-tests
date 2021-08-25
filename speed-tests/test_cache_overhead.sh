aed996840be0f30f367e1df30cba2a30012e7798
Jerin Philip <jphilip@ed.ac.uk>
2021-08-25 09:13:11 +0000

    cache-overhead-metrics added as a script

---
+++ speed-tests/test_cache_overhead.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Usage (example on var): 
+# MARIAN=../bergamot-translator/build TIMEOUT=45m BRT_THREADS=48 BRT_EXPECTED_MAXTIME=500 ./run_brt.sh speed-tests/test_wngt20_perf.sh
+# 
+# Uses the following environment variables additionally.
+# TIMEOUT: 
+#    Normal regression-tests have a timeout of 5m, however WNGT20 takes longer
+#    even with many more cores.
+# 
+# BRT_THREADS:
+#    Number of marian-worker threads to spawn on a test-machine.
+#   
+# BRT_EXPECTED_MAXTIME:
+#   Parameter needs to be set with a reasonable value tuning for hardware.
+#   Repeated development shouldn't compromise the existing speed.
+# 
+# Computes BLEU/performance on WNGT20 continuously to ensure no quality issues
+# arise in continuous development.
+
+set -eo pipefail;
+
+# Skip if requirements are not met
+if [ ! $BRT_MARIAN_USE_MKL ]; then
+    echo "Bergamot translator is not compiled with CPU" 1>&2
+    exit 100
+elif ! grep -q -e "avx" -e "ssse3" /proc/cpuinfo  ; then
+    echo "Your CPU does not support AVX or SSSE3, which is required" 1>&2
+    exit 100
+fi
+
+THREADS=${BRT_THREADS:-16}
+EXPECTED_MAX_TIME=${BRT_EXPECTED_MAXTIME:-2500.00}
+
+if [ `nproc` -lt 16 ]; then
+  echo "Skipping, not enough cores to run this test in feasible time. At least 16 required"
+  exit 100
+fi;
+
+if [ `nproc` -lt ${THREADS} ]; then
+  echo "Skipping, hardware doesn't have enough CPUs to run ${THREADS} threads."
+  exit 100
+fi;
+
+
+INPUT_FILE="$BRT_DATA/wngt20/sources.shuf"
+
+
+function benchmark-parameterized-by-cache {
+    CACHE_ARG="$1"
+    INPUT_FILE="$2"
+    THREADS="$3"
+    TAG="cpu.marian-decoder-new.${THREADS}.cache.${CACHE_ARG}"
+
+    ADDITIONAL_ARGS=(
+        --quiet-translation # Don't want any logs here.
+        --ssplit-mode sentence  # We use line based ssplit, which is faster and no-regex overhead.
+        --cpu-threads ${THREADS}  
+        --log ${TAG}.log -o ${TAG}.translated.log
+        --cache-translations ${CACHE_ARG}
+    )
+
+    time ${BRT_MARIAN}/app/bergamot --bergamot-mode decoder $BRT_FILE_ARGS "${ADDITIONAL_ARGS[@]}" < $INPUT_FILE > ${TAG}.translated.log;
+    WALLTIME=$(tail -n1 -v ${TAG}.log | grep -o "[0-9\.]*s" | sed 's/s//g')
+    echo "WallTime: $WALLTIME"
+}
+
+benchmark-parameterized-by-cache "false" $INPUT_FILE $THREADS
+benchmark-parameterized-by-cache "true" $INPUT_FILE $THREADS
+
+# Do the same, but with single worker thread => no-contention?
+# Only first 100,000 lines
+SMALLER_INPUT_FILE=$(mktemp --suffix ".100k")
+
+head -n 100000 $INPUT_FILE > $SMALLER_INPUT_FILE
+benchmark-parameterized-by-cache "true" $SMALLER_INPUT_FILE 1
+benchmark-parameterized-by-cache "false" $SMALLER_INPUT_FILE 1
+
+
