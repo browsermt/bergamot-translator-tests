71edef34f9fa1652fd71d830d249b72e554a7188
Felipe C. Dos Santos <felipe.santos.k@gmail.com>
2021-09-06 23:36:45 -0300

    Split QE tests and move QE input and output files

--- env.d/base.sh
+++ env.d/base.sh
@@ -30,6 +30,24 @@ COMMON_ARGS=(
     -w 128
 )
 
+BRT_TEST_PACKAGE_EN_ET=${BRT_MODELS}/enet/enet.student.tiny11
+
+COMMON_EN_ET_ARGS=(
+    -m ${BRT_TEST_PACKAGE_EN_ET}/model.intgemm.alphas.bin
+    --vocabs
+        ${BRT_TEST_PACKAGE_EN_ET}/vocab.eten.spm
+        ${BRT_TEST_PACKAGE_EN_ET}/vocab.eten.spm
+    --ssplit-prefix-file
+        ${BRT_TEST_PACKAGE_EN_DE}/nonbreaking_prefix.en
+    --alignment soft
+    --beam-size 1
+    --gemm-precision ${GEMM_PRECISION}
+    --max-length-break 1024
+    --mini-batch-words 1024
+    -w 128
+    --quality ${BRT_TEST_PACKAGE_EN_ET}/quality_model.bin
+)
+
 # Shortlist differs in filename when using bytearray or files.
 # We support both modes bytearray format and also files. 
 
@@ -53,4 +71,5 @@ function brt-bytearray-args {
 
 export BRT_FILE_ARGS=$(brt-file-args)
 export BRT_BYTEARRAY_ARGS=$(brt-bytearray-args)
+export BRT_EN_ET_ARGS=$( echo "${COMMON_EN_ET_ARGS[@]}")
 
