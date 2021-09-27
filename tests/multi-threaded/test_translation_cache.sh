e63be3a70423dda4f78ce2eb12433ba021eb69a8
Jerin Philip <jphilip@ed.ac.uk>
2021-09-27 17:55:11 +0000

    Moving cache steps independent for wasm/native

--- tests/multi-threaded/test_translation_cache.sh
+++ tests/multi-threaded/test_translation_cache.sh
@@ -10,6 +10,6 @@ set -eo pipefail;
 
 # There are no output / expected files here. Just a check if the cache-stats reflect that the cache works correctly, ABORT contained within the test-app if not.
 
-${BRT_MARIAN}/bergamot-test-native --bergamot-mode test-translation-cache ${BRT_FILE_ARGS} < ${BRT_DATA}/simple/bergamot/input.txt
+${BRT_MARIAN}/bergamot-test-native --bergamot-mode test-translation-cache --cache-translations ${BRT_FILE_ARGS} < ${BRT_DATA}/simple/bergamot/input.txt
 
 exit 0
