45901ed48799e2bff0e241403cca7e9e3ba33e05
Jerin Philip <jphilip@ed.ac.uk>
2022-01-14 17:02:01 +0000

    Add a blocking pivoting test as well

--- tests/blocking/test_pivoting.sh
+++ tests/blocking/test_pivoting.sh
@@ -3,7 +3,7 @@
 #####################################################################
 # SUMMARY: Run tests for faithful-translation
 # AUTHOR: jerinphilip 
-# TAGS: full, async
+# TAGS: full, blocking
 #####################################################################
 
 set -eo pipefail;
@@ -12,14 +12,13 @@ ARGS=(
     --model-config-paths 
         "$BRT_TEST_PACKAGE_EN_ES/config.intgemm8bitalpha.yml.bergamot.yml" # forward
         "$BRT_TEST_PACKAGE_ES_EN/config.intgemm8bitalpha.yml.bergamot.yml" # backward
-    --cpu-threads 4
 )
 
 # Generate output specific to hardware.
 OUTFILE=${BRT_DATA}/simple/bergamot/$(brt_outfile "pivoting")
 EXPECTED=${BRT_DATA}/simple/bergamot/$(brt_expected "pivoting")
 
-${BRT_MARIAN}/tests/async --bergamot-mode test-pivot "${ARGS[@]}" < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
+${BRT_MARIAN}/tests/blocking --bergamot-mode test-pivot "${ARGS[@]}" < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
 
 # Compare with output specific to hardware.
 if [[ "$BRT_EVAL_MODE" == "approx" ]]; then
