54528dcc30e9198c00a3e9a1eb7eddda48d1115b
Jerin Philip <jphilip@ed.ac.uk>
2022-02-09 19:05:33 +0000

    Choose a different name for outfile to avoid overwrite

--- tests/cli/test_npz_load.sh
+++ tests/cli/test_npz_load.sh
@@ -9,7 +9,7 @@
 set -eo pipefail;
 
 # Generate output specific to hardware.
-OUTFILE=${BRT_DATA}/simple/bergamot/$(brt_outfile "faithful-translation")
+OUTFILE=${BRT_DATA}/simple/bergamot/$(brt_outfile "faithful-translation-with-npz")
 EXPECTED=${BRT_DATA}/simple/bergamot/$(brt_expected "faithful-translation")
 ${BRT_MARIAN}/app/bergamot --model-config-paths "$BRT_TEST_PACKAGE_EN_DE/config.yml.bergamot.yml" \
         < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
