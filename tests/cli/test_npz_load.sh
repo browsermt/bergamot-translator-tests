3c0f95a1775a74f5db441aa2f17ceb7437679022
Jerin Philip <jerinphilip@live.in>
2022-02-09 19:25:23 +0000

    Tests for npz model loads (#57)
    * patch the npz config as well
    * approx eval for npz path is enough
    * They broke sacrebleu with 2.0.0
    * Choose a different name for outfile to avoid overwrite

---
+++ tests/cli/test_npz_load.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#####################################################################
+# SUMMARY: Run tests for npz-load
+# AUTHOR: jerinphilip 
+# TAGS: full, native
+#####################################################################
+
+set -eo pipefail;
+
+# Generate output specific to hardware.
+OUTFILE=${BRT_DATA}/simple/bergamot/$(brt_outfile "faithful-translation-with-npz")
+EXPECTED=${BRT_DATA}/simple/bergamot/$(brt_expected "faithful-translation")
+${BRT_MARIAN}/app/bergamot --model-config-paths "$BRT_TEST_PACKAGE_EN_DE/config.yml.bergamot.yml" \
+        < ${BRT_DATA}/simple/bergamot/input.txt > $OUTFILE
+
+# We will use approx
+$BRT_TOOLS/approx-diff.py $OUTFILE $EXPECTED 
