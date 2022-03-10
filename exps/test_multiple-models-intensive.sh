a4293353aaa9d7471e7da60d2ba186ababeeb42b
Jerin Philip <jphilip@ed.ac.uk>
2022-03-10 16:24:21 +0000

    Add multimodel exps

---
+++ exps/test_multiple-models-intensive.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ARGS=(--model-config-paths "$BRT_TEST_PACKAGE_EN_ES/config.intgemm8bitalpha.yml.bergamot.yml" "$BRT_TEST_PACKAGE_EN_DE/config.intgemm8bitalpha.yml.bergamot.yml" --cpu-threads 2)
+
+$BRT_MARIAN/tests/async --bergamot-mode test-multimodels-intensive ${ARGS[@]} < $BRT_DATA/wngt20/sources.shuf.1k
